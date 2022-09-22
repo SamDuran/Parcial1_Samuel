6bc79371d0b5ff495458e5d8a40c173b7d889584
Samuel Durán <samidt0186@gmail.com>
2022-09-21 20:58:40 -0400

    Initial commit

---
+++ app/src/main/java/edu/ucne/parcial1_samuel/MainActivity.kt
@@ -0,0 +1,27 @@
+package edu.ucne.parcial1_samuel
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import edu.ucne.parcial1_samuel.ui.theme.Parcial1_SamuelTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Parcial1_SamuelTheme {
+                Surface(modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
