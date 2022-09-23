be82a74c5479536627f15bb722e387b4e516cf44
Samuel Durán <samidt0186@gmail.com>
2022-09-23 13:50:29 -0400

    Agregando entidad de prueba a la bd

--- app/src/main/java/edu/ucne/parcial1_samuel/data/local/AppDataBase.kt
+++ app/src/main/java/edu/ucne/parcial1_samuel/data/local/AppDataBase.kt
@@ -2,12 +2,13 @@ package edu.ucne.parcial1_samuel.data.local
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import edu.ucne.parcial1_samuel.data.local.entities.entity
 
 @Database(
-    entities = [],
+    entities = [entity::class],
     exportSchema = false,
     version =1
 )
 abstract class AppDataBase : RoomDatabase() {
-
+    //daos
 }
