9a6c7dcf00f0c950ce896ed6919a76695bf05401
Samuel Durán <samidt0186@gmail.com>
2022-09-28 18:16:32 -0400

    Agregando Dao ArticuloDao.kt

---
+++ app/src/main/java/edu/ucne/parcial1_samuel/data/local/dao/ArticuloDao.kt
@@ -0,0 +1,20 @@
+package edu.ucne.parcial1_samuel.data.local.dao
+
+import androidx.room.*
+import edu.ucne.parcial1_samuel.data.local.entities.Articulo
+
+@Dao
+interface ArticuloDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(articulo: Articulo)
+
+    @Delete
+    suspend fun delete(articulo: Articulo)
+
+    @Query("SELECT * FROM Articulos WHERE articuloId = :id")
+    suspend fun find(id:Int)
+
+    @Query("SELECT * FROM Articulos")
+    suspend fun getArticulos()
+}
